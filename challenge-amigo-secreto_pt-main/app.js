8894810e6f666bf90011e3220d2dae06c85cdafa
Elieslly Nascimento <elieslly1@gmail.com>
2025-08-22 11:53:42 -0300

    Adicionando funções

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo(){
+let amigos = document.querySelector('input').value;
+if(amigos === ''){
+    alert('Por favor, insira um nome.');
+}else{
+listaDeAmigos.push(amigos);
+limparCampo();
+}
+}
+
+function limparCampo() {
+    amigos = document.querySelector('input');
+    amigos.value = '';
+}
+
+console.log(listaDeAmigos);
+
